beb5207a813d84bff51dd6e0eac9d4651cfe6490
Maiwenn <le-goasteller.e2000660@etud.univ-ubs.fr>
2022-02-02 09:43:32 +0100

    Ajout arbo pour angular

---
+++ Meteo_Angular/js/app.js
@@ -0,0 +1,11 @@
+angular.module("meteo", ["ngRoute"]);
+
+angular.module('meteo').config(['$routeProvider',
+    function config($routeProvider) {
+        $routeProvider.when('/villes', {
+            templateUrl: "partials/villes.html"
+        }).when('/previsions', {
+            templateUrl: "partials/previsions.html"
+        });
+    }
+]);
