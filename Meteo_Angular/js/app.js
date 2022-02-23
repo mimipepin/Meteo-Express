ed07d0180ec28983448163bebb80bf9b80f5c2e2
Maiwenn <le-goasteller.e2000660@etud.univ-ubs.fr>
2022-02-23 10:51:15 +0100

    requette simple dans villes (pour tester c'est pas finis)

--- Meteo_Angular/js/app.js
+++ Meteo_Angular/js/app.js
@@ -1,10 +1,11 @@
-angular.module("meteo", ["ngRoute"]);
+let app = angular.module("meteo", ["ngRoute"]);
 
-angular.module('meteo').config(['$routeProvider',
+app.config(['$routeProvider',
     function config($routeProvider) {
         $routeProvider
         .when('/villes', {
-            templateUrl: "partials/villes.html"
+            templateUrl: "partials/villes.html",
+            controller: "testController"
         }).when('/previsions', {
             templateUrl: "partials/previsions.html"
         }).when('/meteovilles', {
@@ -12,3 +13,35 @@ angular.module('meteo').config(['$routeProvider',
         });
     }
 ]);
+
+// récupère la valeur de l'input
+// app.controller("testController", ["$scope", function($scope) {
+//     $scope.valid = function () {
+//         console.log($scope.city);
+//     }
+//     // $scope.test = "hey";
+//     // console.log("heeey");
+// }]);
+
+// Récup la valeur de l'input et les infos correspondantes quand on appuie sur valider
+app.controller("testController", ["$scope", "$http", function($scope, $http) {
+    $scope.valid = function () {
+        console.log($scope.city);
+        let city = $scope.city;
+        $http({
+            method: 'GET',
+            url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=fr&APPID=2370a9749f38195f07d3bbefd145b74c&units=metric`
+        }).then(function successCallback(response) {
+            // this callback will be called asynchronously
+            // when the response is available
+            console.log(response.data.weather[0].description)
+            $scope.test = response.data.weather[0].description;
+    
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            console.log("pas de ville");
+            throw new Error(error.statusText);
+        });
+    }
+}]);
