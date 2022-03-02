883ba25994eeacc068eca5e5983a8d3cb6fef049
Maiwenn <le-goasteller.e2000660@etud.univ-ubs.fr>
2022-03-02 21:54:32 +0100

    tous pleins de trucs qui marchent bien

--- Meteo_Angular/js/app.js
+++ Meteo_Angular/js/app.js
@@ -6,108 +6,13 @@ app.config(['$routeProvider',
             .when('/villes', {
                 templateUrl: "partials/villes.html",
                 controller: "testController"
-            }).when('/previsions', {
-                templateUrl: "partials/previsions.html/:ville",
+            }).when('/previsions/:ville', {
+                templateUrl: "partials/previsions.html",
                 controller: "previsionsController"
-            }).when('/meteovilles', {
+            }).when('/', {
                 templateUrl: "partials/meteovilles.html",
                 controller: "meteovillesController"
-            });
+            })
+            .otherwise('/');
     }
 ]);
-
-// récupère la valeur de l'input
-// app.controller("testController", ["$scope", function($scope) {
-//     $scope.valid = function () {
-//         console.log($scope.city);
-//     }
-//     // $scope.test = "hey";
-//     // console.log("heeey");
-// }]);
-
-getInfo = function ($http, city) {
-    $http({
-        method: 'GET',
-        url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=fr&APPID=2370a9749f38195f07d3bbefd145b74c&units=metric`
-    }).then(function successCallback(response) {
-        // this callback will be called asynchronously
-        // when the response is available
-        return response;
-
-    }, function errorCallback(response) {
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
-        console.log("pas de ville");
-        throw new Error(error.statusText);
-    });
-}
-
-createArray = function(data){
-    ret = array();
-    ret['title'] = data.name;
-    ret['text'] = data.weather[0].description;
-    ret['temp'] = data.main.temp;
-    ret['vitesseVent'] = data.wind.speed;
-    ret['orientationVent'] = data.wind.deg;
-    ret['icone'] = data.weather[0].icon;
-    ret['humidite'] = data.main.humidity;
-    ret['pression'] = data.main.pressure;
-    return ret;
-}
-
-// permet de passer un argument en paramètre
-// doc : https://docs.angularjs.org/api/ngRoute/service/$route 
-// et https://github.com/angular/angular.js/issues/11063 mais lui il met son controller direct dans le app.config
-app.controller("meteovillesController", ["$scope", "$http", "$route", function ($scope, $http, $routeParams) {
-    $scope.prev = function () {
-        // quand on clique sur un lien
-        $routeParams.updateParams({ ville: "Vannes" });
-    }
-    $scope.stored = [
-        //On retrouve ici les informations météo "en dur", que l'on devra remplacer par les résultats de requêtes à l'API plus tard
-        //TODO
-        { title: 'Vannes', text: "couvert", temp:"20°C",vitesseVent:"50km/h",orientationVent:"Nord-Ouest",icone:"wi wi-owm-800",humidite:"12%", pression:"1 HPA"  },
-        { title: 'Shangaï', text: "ensoleillé", temp:"22°C",vitesseVent:"72km/h",orientationVent:"Nord-Est",icone:"wi wi-owm-800",humidite:"14%", pression:"1 HPA"  },
-        { title: 'Jérusalem', text: "couvert" , temp:"36°C",vitesseVent:"13km/h",orientationVent:"Sud-Est",icone:"wi wi-owm-800",humidite:"26%", pression:"1 HPA"  }
-    ];
-
-
-}])
-
-app.controller("previsionsController", ["$scope", "$http", "$routeParams", function ($scope, $http, $routeParams) {
-    console.log($routeParams.ville);
-    info = getInfo($http, $routeParams.ville);
-    console.log(info.weather[0].description);
-}])
-
-
-
-// Récup la valeur de l'input et les infos correspondantes quand on appuie sur valider
-app.controller("testController", ["$scope", "$http", function ($scope, $http) {
-    $scope.valid = function () {
-        console.log($scope.city);
-        let city = $scope.city;
-        $http({
-            method: 'GET',
-            url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=fr&APPID=2370a9749f38195f07d3bbefd145b74c&units=metric`
-        }).then(function successCallback(response) {
-            // this callback will be called asynchronously
-            // when the response is available
-            console.log(response.data);
-            var a = createArray(response.data);
-            localStorage.setItem($scope.city, a);
-
-
-            $scope.test = response.data.weather[0].description;
-
-        }, function errorCallback(response) {
-            // called asynchronously if an error occurs
-            // or server returns response with an error status.
-            console.log("pas de ville");
-            throw new Error(error.statusText);
-        });
-        
-    }
-        
-}]);
-
--- Meteo_Angular/js/controllers/controller.js
+++ Meteo_Angular/js/controllers/controller.js
@@ -1,38 +1,86 @@
-// function controllerMeteo($scope) {
-//     console.log("ici");
-//     $scope.test = "hey";
-// }
+let createArray = function(data){
+    ret = {};
+    ret['title'] = data.name;
+    ret['text'] = data.weather[0].description;
+    ret['temp'] = data.main.temp;
+    ret['vitesseVent'] = data.wind.speed;
+    ret['orientationVent'] = data.wind.deg;
+    ret['icone'] = data.weather[0].icon;
+    ret['humidite'] = data.main.humidity;
+    ret['pression'] = data.main.pressure;
+    return ret;
+}
 
-// function getMeteo ($scope) {
-//     $http({
-//         method: 'GET',
-//         url: `https://api.openweathermap.org/data/2.5/weather?q=vannes&lang=fr&APPID=ee07e2bf337034f905cde0bdedae3db8&units=metric`
-//     }).then(function successCallback(response) {
-//         // this callback will be called asynchronously
-//         // when the response is available
-//         console.log("yay");
-//         console.log(response);
+// permet de passer un argument en paramètre
+// doc : https://docs.angularjs.org/api/ngRoute/service/$route 
+// et https://github.com/angular/angular.js/issues/11063 mais lui il met son controller direct dans le app.config
+app.controller("meteovillesController", ["$scope", "$http", function ($scope, $http) {
+    $scope.villes = [{title:"Vannes", id: 0}, {title:"Shangai", id: 1}, {title:"Jerusalem", id: 2}];
+    $scope.villes.forEach(v => {
+        $http.get(`https://api.openweathermap.org/data/2.5/weather?q=${v.title}&APPID=2370a9749f38195f07d3bbefd145b74c&units=metric&lang=fr`)
+        .then(res => {
+            v.data = {};
+            v.data.temp = res.data.main.temp;
+            v.data.desc = res.data.weather[0].description;
+            v.data.vVent = res.data.wind.speed;
+            v.data.orVent = `wi wi-wind towards-${res.data.wind.deg}-deg`;
+            v.data.meteo = `wi wi-owm-${res.data.weather[0].id}`
+            v.data.humidite = res.data.main.humidity;
+            v.data.pression = res.data.main.pressure;
+        });
+    });
+}])
 
-//     }, function errorCallback(response) {
-//         // called asynchronously if an error occurs
-//         // or server returns response with an error status.
-//         console.log("pas de ville");
-//         throw new Error(error.statusText);
-//     });
-// }
+app.controller("previsionsController", ["$scope", "$http", "$routeParams", function ($scope, $http, $routeParams) {
+    let villes = [{title: "Vannes", id:0}, {title:"Shangai", id: 1}, {title:"Jerusalem", id: 2}];
+    $scope.ville = villes.find(elem => elem.id.toString() === $routeParams.ville); // sort l'objet ville pour lequel l'id correspond à celui passé en param
+    $scope.title = $scope.ville.title;
+    $scope.ville.data = [];
+    $http.get(`https://api.openweathermap.org/geo/1.0/direct?q=${$scope.ville.title},fr&limit=1&appid=2370a9749f38195f07d3bbefd145b74c`)
+        .then(cooRes => {
+            $http.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${cooRes.data[0].lat}&lon=${cooRes.data[0].lon}&exclude=current,minutely,hourly,alerts&appid=2370a9749f38195f07d3bbefd145b74c&units=metric&lang=fr`)
+            .then(res => {
+                //pour chaque jour, ajoute un objet avec les infos
+                res.data.daily.forEach(elem => {
+                    $scope.ville.data.push({
+                        date: new Intl.DateTimeFormat('fr-FR', { dateStyle: 'full'}).format(new Date(elem.dt*1000)),
+                        desc: elem.weather[0].description,
+                        meteo: `wi wi-owm-${elem.weather[0].id}`,
+                        min: elem.temp.min,
+                        max: elem.temp.max
+                    });
+                });
+            });
+        });
+}])
 
-// function test ($scope) {
-//     console.log("teeeeeeeeeeeest");
-//     $scope.test = "hey";
-// }
 
-// angular.module("meteo", []) 
-// .controller("controlMeteo", test);
 
-// let controlMeteo = angular.module("mainController", []);
-// controlMeteo.controller("mainController", ["$scope", function($scope) {
-//     $scope.test = "hey";
-//     console.log("heeey");
-// }]);
+// Récup la valeur de l'input et les infos correspondantes quand on appuie sur valider
+app.controller("testController", ["$scope", "$http", function ($scope, $http) {
+    $scope.valid = function () {
+        console.log($scope.city);
+        let city = $scope.city;
+        $http({
+            method: 'GET',
+            url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=fr&APPID=2370a9749f38195f07d3bbefd145b74c&units=metric`
+        }).then(function successCallback(response) {
+            // this callback will be called asynchronously
+            // when the response is available
+            console.log(response.data);
+            var a = createArray(response.data);
+            localStorage.setItem($scope.city, a);
 
+            $scope.test = response.data.weather[0].description;
+
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            console.log("pas de ville");
+            throw new Error(error.statusText);
+        });
+        
+    }
+        
+}]);
 
