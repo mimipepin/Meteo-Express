5ac7e75ddc0436526f6b1368c204a05e28af0504
Héloïse Lalande <lalande.e2002293@etud.univ-ubs.fr>
2022-03-02 11:03:28 +0100

    local storage et formatage de carte

--- Meteo_Angular/js/app.js
+++ Meteo_Angular/js/app.js
@@ -3,16 +3,16 @@ let app = angular.module("meteo", ["ngRoute"]);
 app.config(['$routeProvider',
     function config($routeProvider) {
         $routeProvider
-        .when('/villes', {
-            templateUrl: "partials/villes.html",
-            controller: "testController"
-        }).when('/previsions', {
-            templateUrl: "partials/previsions.html/:ville",
-            controller: "previsionsController"
-        }).when('/meteovilles', {
-            templateUrl: "partials/meteovilles.html",
-            controller: "meteovillesController"
-        });
+            .when('/villes', {
+                templateUrl: "partials/villes.html",
+                controller: "testController"
+            }).when('/previsions', {
+                templateUrl: "partials/previsions.html/:ville",
+                controller: "previsionsController"
+            }).when('/meteovilles', {
+                templateUrl: "partials/meteovilles.html",
+                controller: "meteovillesController"
+            });
     }
 ]);
 
@@ -42,21 +42,36 @@ getInfo = function ($http, city) {
     });
 }
 
+createArray = function(data){
+    ret = array();
+    ret['title'] = data.name;
+    ret['text'] = data.weather[0].description;
+    ret['temp'] = data.main.temp;
+    ret['vitesseVent'] = data.wind.speed;
+    ret['orientationVent'] = data.wind.deg;
+    ret['icone'] = data.weather[0].icon;
+    ret['humidite'] = data.main.humidity;
+    ret['pression'] = data.main.pressure;
+    return ret;
+}
+
 // permet de passer un argument en paramètre
 // doc : https://docs.angularjs.org/api/ngRoute/service/$route 
 // et https://github.com/angular/angular.js/issues/11063 mais lui il met son controller direct dans le app.config
 app.controller("meteovillesController", ["$scope", "$http", "$route", function ($scope, $http, $routeParams) {
     $scope.prev = function () {
         // quand on clique sur un lien
-        $routeParams.updateParams({ville: "Vannes"});
+        $routeParams.updateParams({ ville: "Vannes" });
     }
     $scope.stored = [
-        { title: 'Vannes', text: "Une petite ville de Bretagne" },
-        { title: 'Shangaï', text: "Une métropole chinoise" },
-        { title: 'Jérusalem', text: "WE WILL TAKE IT" }
-      ];
+        //On retrouve ici les informations météo "en dur", que l'on devra remplacer par les résultats de requêtes à l'API plus tard
+        //TODO
+        { title: 'Vannes', text: "couvert", temp:"20°C",vitesseVent:"50km/h",orientationVent:"Nord-Ouest",icone:"wi wi-owm-800",humidite:"12%", pression:"1 HPA"  },
+        { title: 'Shangaï', text: "ensoleillé", temp:"22°C",vitesseVent:"72km/h",orientationVent:"Nord-Est",icone:"wi wi-owm-800",humidite:"14%", pression:"1 HPA"  },
+        { title: 'Jérusalem', text: "couvert" , temp:"36°C",vitesseVent:"13km/h",orientationVent:"Sud-Est",icone:"wi wi-owm-800",humidite:"26%", pression:"1 HPA"  }
+    ];
 
-    
+
 }])
 
 app.controller("previsionsController", ["$scope", "$http", "$routeParams", function ($scope, $http, $routeParams) {
@@ -66,8 +81,9 @@ app.controller("previsionsController", ["$scope", "$http", "$routeParams", funct
 }])
 
 
+
 // Récup la valeur de l'input et les infos correspondantes quand on appuie sur valider
-app.controller("testController", ["$scope", "$http", function($scope, $http) {
+app.controller("testController", ["$scope", "$http", function ($scope, $http) {
     $scope.valid = function () {
         console.log($scope.city);
         let city = $scope.city;
@@ -77,15 +93,21 @@ app.controller("testController", ["$scope", "$http", function($scope, $http) {
         }).then(function successCallback(response) {
             // this callback will be called asynchronously
             // when the response is available
-            console.log(response.data.weather[0].description)
+            console.log(response.data);
+            var a = createArray(response.data);
+            localStorage.setItem($scope.city, a);
+
+
             $scope.test = response.data.weather[0].description;
-    
+
         }, function errorCallback(response) {
             // called asynchronously if an error occurs
             // or server returns response with an error status.
             console.log("pas de ville");
             throw new Error(error.statusText);
         });
+        
     }
+        
 }]);
 
